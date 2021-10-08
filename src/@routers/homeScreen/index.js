a5baab07ed9e41dc41d9122ee5dd474c583674a5
mobi1992 <mobeenamushtaq2@gmail.com>
2021-10-08 10:25:49 +0500

    changes5

--- src/@routers/homeScreen/index.js
+++ src/@routers/homeScreen/index.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import {BrowserRouter as Router, Switch, Route, Link, useHistory} from 'react-router-dom'
 import {apis} from '../../@services'
+import DocItem from './doc-item'
+import './styles.css'
 const HomeScreen = () => {
     const history = useHistory()
     const [loading, setLoading] = useState(false);
@@ -28,11 +30,36 @@ const HomeScreen = () => {
             setLoading(false);
         }
     }
-    return (
-        <div>
-            
-        </div>
-    )
+
+
+    if(data.length === 0 && !loading)
+    {
+        return (
+            <div className = 'container'>
+                <div className = 'row justify-content-center align-items-center pos-fixed'>
+                    <div className = 'col-lg-8 col-md-8 pos-rlv'>
+                        <div className = 'card card-body border-0'>
+                            <h2 className = 'text-center'>Welcome to MedicalReel</h2>
+                            <div className = 'text-center'>Please start adding pictures or documents to add to your reel</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else 
+    {
+        return (
+            <div>
+                {
+               data.map(item => (
+                   
+                    <DocItem item = {item}/>
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
 export default HomeScreen
